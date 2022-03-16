fff4e76ea96603212950dd52dcb6f613b8c16681
sath00 <91469351+sath00@users.noreply.github.com>
2022-03-16 11:37:17 +0800

    first commit

---
+++ models/Cart.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+    userId: {type: String, required: true, unique:true},
+    products: [
+        {
+            productId:{
+                type: String
+            },
+            quantity: {
+                type: Number,
+                default: 1,
+            },
+        },
+    ],
+    amount: {type: Number, required: true},
+},
+{timestamps: true}
+);
+
+module.exports = mongoose.model("Cart", CartSchema )
